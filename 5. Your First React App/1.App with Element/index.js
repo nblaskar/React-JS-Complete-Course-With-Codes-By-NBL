1cb947fb65e50fc38f99a8c0b925f2912783bb2c
NUR BAHAR LASKAR <42918029+nblaskar@users.noreply.github.com>
2020-11-12 16:51:17 +0530

    React

---
+++ 5. Your First React App/1.App with Element/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// let el=React.createElement("h1",null,"Hello React Application");
+let el = <h1>Hello React Application</h1>;
+ReactDOM.render(el, document.getElementById("root"));
